69ed674e3c22d07353aab9c75d29773638a105b4
JanAwadallah <32688971+JanAwadallah@users.noreply.github.com>
2021-12-10 20:30:19 +1100

    Update ImageCard.jsx

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -7,7 +7,7 @@ const FeedbackItem = () => {
   const [images, setImages] = useState([]);
   const [uploaded, setUploaded] = useState(false);
   const fetchImages = async () => {
-    const url = process.env.BACKEND + "/fetchimages";
+    const url = process.env.REACT_APP_BACKEND + "/fetchimages";
     console.log(url)
     const res = await axios.get(url);
     setImages(res.data);
@@ -22,8 +22,34 @@ const FeedbackItem = () => {
     <div>
       <Upload setUploaded={setUploaded} />
       <div className="cardIMG">
-        {images && <h1>Test</h1>}
-      </div>
+{images &&
+          images.map((item) => (
+            <div className="img-container">
+              {["image/jpg", "image/jpeg", "image/png"].includes(item.type) ? (
+                <img
+                  key={item._id}
+                  style={{
+                    // margin: "5%",
+                    // display: "block",
+                    maxWidth: "100%",
+                    // maxHeight: "100%",
+                    // width: "auto",
+                    // height: "auto",
+                  }}
+                  src={process.env.REACT_APP_LINK + item.filename}
+                  alt="new"
+                />
+              ) : (
+                <Player
+                  style={{ maxWidth: "100%" }}
+                  key={item._id}
+                  playsInline
+         
+                  src={process.env.REACT_APP_LINK + item.filename}
+                />
+              )}
+            </div>
+          ))}      </div>
     </div>
   );
 };
