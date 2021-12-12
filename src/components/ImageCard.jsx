8bde09528ce6c82e95868f2c4b63cd980807248e
JanAwadallah <32688971+JanAwadallah@users.noreply.github.com>
2021-12-13 00:33:36 +1100

    Update ImageCard.jsx

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -3,71 +3,82 @@ import axios from "axios";
 import { Player } from "video-react";
 import Upload from "./Upload";
 import CloseIcon from "@material-ui/icons/Close";
+import Slider from "./Slider";
 const FeedbackItem = () => {
   const [images, setImages] = useState([]);
+  const [length, setLength] = useState(0);
+  const [fetchStatus, setFetchStatus] = useState(null);
   const [uploaded, setUploaded] = useState(false);
   const [model, setModel] = useState(false);
   const [tempSrc, setTempSrc] = useState("");
   const [tempType, setTempType] = useState("");
+  const [tempIndex, setTempIndex] = useState(0);
   const fetchImages = async () => {
     const res = await axios.get(process.env.REACT_APP_FETCHIMAGES);
+    setFetchStatus(res.status);
     setImages(res.data);
+    setLength(res.data.length);
   };
   useEffect(() => {
     fetchImages();
   }, [uploaded]);
-  const getImg = (imgSrc, type) => {
+  const getImg = (imgSrc, type, index) => {
     setTempSrc(imgSrc);
     setTempType(type);
     setModel(true);
+    setTempIndex(index);
   };
-  console.log(tempSrc);
 
   return (
     <>
-      <div className={model ? "open" : "model"}>
-        {["image/jpg", "image/jpeg", "image/png"].includes(tempType) ? (
-          <div>
-            <img src={tempSrc} alt="new" />
-            <CloseIcon onClick={() => setModel(false)} />
-          </div>
-        ) : null}
-      </div>
+      <Slider
+        setModel={setModel}
+        images={images}
+        type={tempType}
+        tempIndex={tempIndex}
+        model={model}
+        length={length}
+        setTempIndex={setTempIndex}
+      />
+
       <div>
         <Upload setUploaded={setUploaded} />
         <div className="cardIMG">
           {images &&
-            images.map((item) => (
-              <div className="img-container">
-                {["image/jpg", "image/jpeg", "image/png"].includes(
-                  item.type
-                ) ? (
-                  <img
-                    key={item._id}
-                    style={{
-                      width: "100%",
-                    }}
-                    src={process.env.REACT_APP_LINK + item.filename}
-                    alt="new"
-                    onClick={() =>
-                      getImg(
-                        process.env.REACT_APP_LINK + item.filename,
-                        item.type
-                      )
-                    }
-                  />
-                ) : (
-                  <div style={{ marginBottom: 6 }}>
-                    <Player
-                      style={{ width: "100%" }}
+            images.map((item, index) => {
+              return (
+                <div className="img-container">
+                  {["image/jpg", "image/jpeg", "image/png"].includes(
+                    item.type
+                  ) ? (
+                    <img
                       key={item._id}
-                      playsInline
+                      style={{
+                        width: "100%",
+                      }}
                       src={process.env.REACT_APP_LINK + item.filename}
+                      alt="new"
+                      onClick={() =>
+                        getImg(
+                          process.env.REACT_APP_LINK + item.filename,
+                          item.type,
+                          index
+                        )
+                      }
                     />
-                  </div>
-                )}
-              </div>
-            ))}
+                  ) : (
+                    <div style={{ marginBottom: 6 }}>
+                      <Player
+                        style={{ width: "100%" }}
+                        key={item._id}
+                        playsInline
+                        src={process.env.REACT_APP_LINK + item.filename}
+                      />
+                    </div>
+                  )}
+                </div>
+              );
+            })}
         </div>
       </div>
     </>
